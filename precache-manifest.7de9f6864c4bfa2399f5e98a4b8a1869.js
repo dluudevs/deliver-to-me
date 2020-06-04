c2885aacc98c46dbc5138e0a35d3c123a49445bd
Derek <derekluu90@gmail.com>
2020-06-03 23:54:07 -0400

    Updates

---
+++ precache-manifest.7de9f6864c4bfa2399f5e98a4b8a1869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585cba9bd77ac33cf3284dedfed8ebdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff917983b2ba71ac2827",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d43ca62b88ca80fe7b49",
+    "url": "/static/js/2.7929c533.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7929c533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff917983b2ba71ac2827",
+    "url": "/static/js/main.de74f0f2.chunk.js"
+  },
+  {
+    "revision": "9bf358e29aad1bc0b1e8",
+    "url": "/static/js/runtime-main.0864c114.js"
+  }
+]);
